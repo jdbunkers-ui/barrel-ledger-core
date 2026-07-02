24ecf1edeb5960eb981c3c39c1c4436e275cb9c0
jdbunkers-ui <j.d.bunkers@gmail.com>
2026-07-02 11:16:30 +0000

    Disable logout link prefetch

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,32 +1,32 @@
 import Link from "next/link";
 import { getCurrentMember } from "@/lib/admin";
- 
+
 export default async function Navigation() {
   const member = await getCurrentMember();
   const isLoggedIn = !!member;
- 
+
   return (
     <nav className="border-b border-stone-300 bg-white">
       <div className="mx-auto flex max-w-6xl gap-10 px-6 py-4 text-lg">
         <Link href="/" className="hover:underline">
           Reviews
         </Link>
- 
+
         <Link href="/producers" className="hover:underline">
           Producers
         </Link>
- 
+
         <Link href="/pickers" className="hover:underline">
           Pickers
         </Link>
- 
+
         {isLoggedIn ? (
           <>
             <Link href="/dashboard" className="hover:underline">
               Dashboard
             </Link>
- 
-            <Link href="/logout" className="hover:underline">
+
+            <Link href="/logout" prefetch={false} className="hover:underline">
               Logout
             </Link>
           </>
@@ -35,7 +35,7 @@ export default async function Navigation() {
             <Link href="/about" className="hover:underline">
               About
             </Link>
- 
+
             <Link href="/login" className="hover:underline">
               Login
             </Link>
